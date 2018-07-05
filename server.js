32135de8083125d0ae8deb087c8eab161fcd0604
aram <boiso.ska8@gmail.com>
2018-07-05 16:55:46 -0500

    hshs

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist'));
 
 app.get('/', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/frontend/index.html'));
+res.render(path.join(__dirname+'/dist/frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
